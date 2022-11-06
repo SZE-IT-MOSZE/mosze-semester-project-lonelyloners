47416b77a14afb871503e7a430528acfd0da4e34
nemethmiklos1996 <nemeth.miklos.1996@gmail.com>
2022-11-06 19:40:59 +0100

    WIP: Égetett szöveg nagyobb vászonnal (teszt)

--- game/src/RenderWindow.cpp
+++ game/src/RenderWindow.cpp
@@ -252,42 +252,31 @@ SDL_Renderer* RenderWindow::getRenderer()
  */
 void RenderWindow::renderText()
 {
-    //this opens a font style and sets a size
-    TTF_Font* Sans = TTF_OpenFont("font/times-new-roman.ttf", 24);
+    TTF_Font* Sans = TTF_OpenFont("font/starcruiser.ttf", 24);
 
-    // this is the color in rgb format,
-    // maxing out all would give you the color white,
-    // and it will be your text's color
-    SDL_Color White = {255, 255, 255};
+    SDL_Color White = {0, 0, 0};
 
     // as TTF_RenderText_Solid could only be used on
     // SDL_Surface then you have to create the surface first
     SDL_Surface* surfaceMessage =
-        TTF_RenderText_Solid(Sans, "LyRs kalandjai", White); 
+        TTF_RenderText_Solid(Sans, "Udvozollek a LonelyLoners nevu", White); 
 
-    // now you can convert it into a texture
     SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
 
-    SDL_Rect Message_rect; //create a rect
-    Message_rect.x = 489;  //controls the rect's x coordinate 
-    Message_rect.y = 85; // controls the rect's y coordinte
-    Message_rect.w = 100; // controls the width of the rect
-    Message_rect.h = 25; // controls the height of the rect
+    // létrehoz egy téglalapot
+    SDL_Rect Message_rect;
+    // beállítja a téglalap x koordinátáját  
+    Message_rect.x = 464;    
+    // beállítja a téglalap x koordinátáját
+    Message_rect.y = 55;    
+    // beállítja a téglalap szélességét
+    Message_rect.w = 240;
+    // beállítja a téglalap magasságát   
+    Message_rect.h = 24;    
 
-    // (0,0) is on the top left of the window/screen,
-    // think a rect as the text's box,
-    // that way it would be very simple to understand
-
-    // Now since it's a texture, you have to put RenderCopy
-    // in your game loop area, the area where the whole code executes
-
-    // you put the renderer's name first, the Message,
-    // the crop size (you can ignore this if you don't want
-    // to dabble with cropping), and the rect which is the size
-    // and coordinate of your texture
     SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
 
-    // Don't forget to free your surface and texture
+    // memória felszabadítása
     SDL_FreeSurface(surfaceMessage);
     SDL_DestroyTexture(Message);
 }
