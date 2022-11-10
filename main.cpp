7b2b2dc6075651e6d85d5cbc8b09bcf0c67e69e3
BalintCs <balint.csefalvay@gmail.com>
2022-11-10 12:39:22 +0100

    Parancs iranyitas

--- main.cpp
+++ main.cpp
@@ -3,21 +3,4 @@
 
 #include "src/headers/Command.h"
 #include "src/headers/Router.h"
-#include "src/headers/Player.h"
-
-int main() {
-    std::string input;
-
-    getline(std::cin, input);
-
-    Player p;
-    Router rt(p);
-
-    if(input != "q") {
-        Command cm(input);
-        cm.make();
-        rt.route(cm.getCommand(), cm.getItem());
-    }
-
-    return 0;
-}
+#in
