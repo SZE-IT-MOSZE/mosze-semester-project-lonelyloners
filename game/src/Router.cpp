96173c17985e0e69a90fd6e1ecc28de817315cc8
BalintCs <balint.csefalvay@gmail.com>
2022-11-17 14:27:42 +0100

    DOXYGEN kommentek

--- game/src/Router.cpp
+++ game/src/Router.cpp
@@ -1,21 +1,63 @@
 #include "headers/Player.h"
 #include "headers/Router.h"
 
-#include <iostream> //törölhető
+#include <iostream>
 
+/**
+ * \brief Parancsot végrehajtó függvény meghívása.
+ * 
+ * A parancs alapján meghívja a parancsnak megfelelő függvényt a megfelelő paraméterekkel.
+ * 
+ * \param cm A parancs szövegkódja.
+ * \param it A tárgy szövegkódja.
+ * \param ent Egy Entity objektum, amelyet a mozgás parancsok használnak.
+ * \param rw Egy RenderWindow objektum, amely a kiíró parancsok használatához kell.
+ */
 
-void Router::route(std::string cm, std::string it, Entity& ent) {
+void Router::route(std::string cm, std::string it, Entity& ent, RenderWindow& rw) {
     if(cm == "FEL" || cm == "LE" || cm == "JOBBRA" || cm == "BALRA" || cm == "ELORE" || cm == "HATRA") {
         this->move(cm, it, ent);
     }
     else if(cm == "HELP" || cm == "SEGITSEG") {
         this->help();
     }
+    else if(cm == "LAPOZ") {
+        this->next_page(rw);
+    }
 }
+
+/**
+ * \brief Lapozó parancs.
+ * 
+ * Meghívja a RenderWindow lapozáshoz szükséges függvényét.
+ * 
+ * \param rw Egy RenderWindow objektum, amely nextPage() függvénye meghívható.
+ */
+
+void Router::next_page(RenderWindow& rw) {
+    rw->nextPage();
+}
+
+/**
+ * \brief Segítség kiírása.
+ * 
+ * Kiírja az engedélyezett parancsok listáját.
+ * 
+ */
 
 void Router::help() {
     std::cout << "Engedelyezett parancsok:" << std::endl;
 }
+
+/**
+ * \brief Játékos mozgatását végzi.
+ * 
+ * A játékost mozgató parancsok alapján átad két paramétert egy entitás objektumnak, amely azt a pozíciót határozza meg, ahova a játékos lépni szeretne.
+ * 
+ * \param cm A parancs szövegkódja.
+ * \param it A tárgy szövegkódja.
+ * \param ent Egy Entity objektum, amelyet a mozgás parancsok használnak.
+ */
 
 void Router::move(std::string cm, std::string it, Entity& ent) {
     int dist = std::stoi(it);
