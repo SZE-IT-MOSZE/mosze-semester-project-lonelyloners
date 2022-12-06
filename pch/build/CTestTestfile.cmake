0f33390e8b6e3ab51ed0e1db83d2bab9c81ba432
Baluka667 <72005798+Baluka667@users.noreply.github.com>
2022-12-06 19:17:34 +0100

    build törlés

--- pch/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/GitHub/mosze-semester-project-lonelyloners/pch
-# Build directory: D:/GitHub/mosze-semester-project-lonelyloners/pch/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("tests")
--- pch/build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/GitHub/mosze-semester-project-lonelyloners/pch/googletest/googlemock
-# Build directory: D:/GitHub/mosze-semester-project-lonelyloners/pch/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
